b6c4e2719208c47f47d10d11f075e65dc017f3ba
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-04-22 18:40:54 -0300

    Remove frontend code and assets for phonebook application

--- part2/phonebook/src/components/MessageAddContact.jsx
+++
@@ -1,9 +0,0 @@
-
-const MessageAddContact = ({ text }) => {
-
-  return (
-    <h2 className='added'>{text}</h2>
-  )
-}
-
-export default MessageAddContact
--- part3/backend/src/index.js
+++
@@ -1,6 +0,0 @@
-import { server } from "./server.js";
-
-const PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/api/persons`);
-});
