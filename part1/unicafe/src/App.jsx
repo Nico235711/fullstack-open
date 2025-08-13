ab87d3e951dd9627807c1b0eb4739afa0a50eb60
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-08-12 23:08:40 -0300

    add statisticLine component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,14 +7,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? "0 %" : `${(good / all) * 100} %`
 
   const increaseGoodButton = () => setGood(good + 1)
   const increaseNeutralButton = () => setNeutral(neutral + 1)
   const increaseBadButton = () => setBad(bad + 1)
-  const hasFeedback = good === 0 && neutral === 0 && bad === 0
 
   return (
     <div>
@@ -23,18 +19,11 @@ const App = () => {
       <Button text={"neutral"} onclick={increaseNeutralButton} />
       <Button text={"bad"} onclick={increaseBadButton} />
       <h2>statistics</h2>
-      {hasFeedback ? (
-        <p>No feedback given</p>
-      ) : (
-        <>
-          <Statistics text={"good"} value={good} />
-          <Statistics text={"neutral"} value={neutral} />
-          <Statistics text={"bad"} value={bad} />
-          <Statistics text={"all"} value={all} />
-          <Statistics text={"average"} value={average} />
-          <Statistics text={"positive"} value={positive} />
-        </>
-      )}
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad} 
+      />
     </div>
   )
 }
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -1,8 +1,20 @@
+import StatisticLine from "./StatisticLine"
 
-const Statistics = ({ text, value }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const hasFeedback = good === 0 && neutral === 0 && bad === 0
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? "0 %" : `${(good / all) * 100} %`
 
   return (
-    <p>{text} {value}</p>
+    <div>
+      <StatisticLine text={"good"} value={good} />
+      <StatisticLine text={"neutral"} value={neutral} />
+      <StatisticLine text={"bad"} value={bad} />
+      <StatisticLine text={"all"} value={all} />
+      <StatisticLine text={"average"} value={average} />
+      <StatisticLine text={"positive"} value={positive} />
+    </div>
   )
 }
 
