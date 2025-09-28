eaba6b8b827c1ab9d497a6809a1a6d7ea4be9d57
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-09-27 22:45:05 -0300

    feat: restructure course data model and implement Course component for better organization

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,12 +1,11 @@
 import Part from "./Part";
 
 export default function Content({ parts }) {
-  const { name, exercises } = parts
 
   return (
     <div>
       {parts.map(part => (
-        <Part key={part.name} name={part.name} exercises={part.exercises} />
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
       ))}
     </div>
   )
--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -2,5 +2,5 @@
 export default function Total({ parts }) {
   const total = parts.reduce((accu, part) => accu + part.exercises, 0)
 
-  return <p>Number of exercises {total}</p>
+  return <b>Number of exercises {total}</b>
 }
